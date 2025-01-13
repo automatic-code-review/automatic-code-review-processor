1d113bb527305de8b7aa35d5f6308e095d001813
kielsonzinn <kielsonzinn@gmail.com>
2025-01-13 14:31:21 -0300

    refactor: Adiciona flush aos logs para funcionar corretamente as pipe

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import sys
 
 from app import processor_executor
 
+
+class AutoFlush:
+    def __init__(self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def flush(self):
+        self.stream.flush()
+
+
 if __name__ == "__main__":
+    sys.stdout = AutoFlush(sys.stdout)
     exit_code = processor_executor.execute()
     sys.exit(exit_code)
